ea7e01712b9f48372eeb369bb510961fa01a1494
Gray <azhadolfo1@gmail.com>
2023-07-31 12:43:36 +0800

    Adding sql connection string

--- rootVariable.cs
+++ rootVariable.cs
@@ -3,10 +3,15 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Data.SqlClient;
 
 namespace TestingPhase
 {
     internal class rootVariable
     {
+        public static string ConnectionString = "Data Source=WIN-IU3ACLEQUUI;Initial Catalog=vb_crud;Persist Security Info=True;User ID=user3;Password=twainc.";
+
+        public static SqlConnection SqlConnection = new SqlConnection(ConnectionString);
+
     }
 }

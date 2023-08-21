7942a610710e479c63ec18e7147ff9cac29e4cdb
Gray <azhadolfo1@gmail.com>
2023-08-21 15:45:32 +0800

    Removing the wrong class library

---
+++ DmsLibrary/FileDocument.cs
@@ -0,0 +1,15 @@
+﻿namespace DmsLibrary;
+
+public class FileDocument
+{
+    public string FileName { get; set; }
+    public string Department { get; set; }
+    public string Description { get; set; }
+
+    public FileDocument(string filename, string department, string description)
+    {
+        this.FileName = filename;
+        this.Department = department;
+        this.Description = description;
+    }
+}
